9dfe28d93b2acf7c8204d0a86866e27d833e6d76
Gwyn Jones <m@gwnjns.com>
2015-11-02 16:21:15 +0000

    Adding condition to the shortcode JS.

--- js/tna-eventbrite-api.js
+++ js/tna-eventbrite-api.js
@@ -11,10 +11,16 @@ $(document).ready(function() {
     $.get('https://www.eventbriteapi.com/v3/events/search/?sort_by=date&organizer.id=' + organizer + '&token=' + token + '&expand=ticket_classes', function(res) {
         if (res.events.length) {
             var s = "<ul class='tna-event-list'>";
+
+            if(n <= res.events.length) {
+              // Do nothing
+            } else {
+                n = res.events.length;
+            }
+
             for (var i = 0; i < n; i++) {
                 var event = res.events[i];
                 var eventTime = moment(event.start.local).format('dddd Do MMMM YYYY, h:mm a');
-                console.dir(event);
                 if(event.logo) {
 			        var image = "<img src='" + event.logo.url + "' alt='" + event.name.text + "'>";
                 } else {
--- tna-eventbrite-api.php
+++ tna-eventbrite-api.php
@@ -9,26 +9,30 @@
  * License: GPL2
  */
 
-add_action( 'wp_footer', 'tna_ebapi_js' );
-function tna_ebapi_js() {
-	?>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.3/moment.js"></script>
-		<script src="<?php echo plugins_url(); ?>/tna-eventbrite-api/js/tna-eventbrite-api.js"></script>
+add_action('wp_footer', 'tna_ebapi_js');
+function tna_ebapi_js()
+{
+    ?>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.3/moment.js"></script>
+    <script src="<?php echo plugins_url(); ?>/tna-eventbrite-api/js/tna-eventbrite-api.js"></script>
     <script src="<?php echo plugins_url(); ?>/tna-eventbrite-api/js/tna-eventbrite-ga.js"></script>
-	<?php
+    <?php
 }
 
-function tna_ebapi_shortcode($atts) {
-	extract(shortcode_atts(array(
-		'organiser' => 2226699547,
-		'numberevents' => 3
-	   ), $atts));
+function tna_ebapi_shortcode($atts)
+{
+    extract(shortcode_atts(array(
+        'organiser' => 2226699547,
+        'numberevents' => 3
+    ), $atts));
     if ($organiser == 2226699547) {
         $url = 'http://nationalarchives.eventbrite.co.uk/';
     }
-	return '<div id="events" data-org-id="' . $organiser . '" data-number-events="' . $numberevents . '"></div>
+
+    return '<div id="events" data-org-id="' . $organiser . '" data-number-events="' . $numberevents . '"></div>
 	        <div class="no-js"><a href="' . $url . '"></a></div>';
 }
+
 add_shortcode('tna-eventbrite', 'tna_ebapi_shortcode');
 
 ?>
