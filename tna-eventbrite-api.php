443e9221fa81c2b1300f0a2a5d233817510b3fba
Chris Bishop <domingobishop@gmail.com>
2015-11-03 14:56:20 +0000

    ga track class added

--- tna-eventbrite-api.php
+++ tna-eventbrite-api.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: TNA Eventbrite API
- * Plugin URI: https://github.com/nationalarchives
+ * Plugin URI: https://github.com/nationalarchives/tna-eventbrite-api
  * Description: This plugin displays events from Eventbrite into a WordPress website.
  * Version: 1.0.0
  * Author: Chris Bishop
@@ -14,8 +14,8 @@ function tna_ebapi_js()
 {
     ?>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.3/moment.js"></script>
-    <script src="<?php echo plugins_url(); ?>/tna-eventbrite-api/js/tna-eventbrite-api.js"></script>
-    <script src="<?php echo plugins_url(); ?>/tna-eventbrite-api/js/tna-eventbrite-ga.js"></script>
+    <script src="<?php echo plugin_dir_url( __FILE__ ); ?>js/tna-eventbrite-api.js"></script>
+    <script src="<?php echo plugin_dir_url( __FILE__ ); ?>js/tna-eventbrite-ga.js"></script>
     <?php
 }
 
@@ -29,7 +29,7 @@ function tna_ebapi_shortcode($atts)
         $url = 'http://nationalarchives.eventbrite.co.uk/';
     }
 
-    return '<div id="events" data-org-id="' . $organiser . '" data-number-events="' . $numberevents . '"></div>
+    return '<div id="events" class="track-outbound" data-org-id="' . $organiser . '" data-number-events="' . $numberevents . '"></div>
 	        <div class="no-js"><a href="' . $url . '"></a></div>';
 }
 
