7f430470f24a0cf94513431fe2c41416c923c4bb
Chris Bishop <domingobishop@gmail.com>
2017-08-10 15:50:18 +0100

    all function_exists tests

--- tests/FunctionsTest.php
+++ tests/FunctionsTest.php
@@ -7,4 +7,48 @@ class FunctionsTest extends PHPUnit_Framework_TestCase
         $this->assertTrue(true);
         $this->assertFalse(false);
     }
+
+    public function test_tna_ebapi_settings()
+    {
+        $this->assertTrue(function_exists('tna_ebapi_settings'));
+    }
+	public function test_tna_ebapi_settings_data()
+	{
+		$this->assertTrue(function_exists('tna_ebapi_settings_data'));
+	}
+	public function test_tna_ebapi_settings_page()
+	{
+		$this->assertTrue(function_exists('tna_ebapi_settings_page'));
+	}
+
+	public function test_tna_ebapi_url()
+	{
+		$this->assertTrue(function_exists('tna_ebapi_url'));
+	}
+	public function test_tna_ebapi_get_eventbrite_json()
+	{
+		$this->assertTrue(function_exists('tna_ebapi_get_eventbrite_json'));
+	}
+	public function test_tna_ebapi_events()
+	{
+		$this->assertTrue(function_exists('tna_ebapi_events'));
+	}
+	public function test_tna_edapi_event_status()
+	{
+		$this->assertTrue(function_exists('tna_edapi_event_status'));
+	}
+
+	public function test_tna_ebapi_css()
+	{
+		$this->assertTrue(function_exists('tna_ebapi_css'));
+	}
+	public function test_tna_ebapi_js()
+	{
+		$this->assertTrue(function_exists('tna_ebapi_js'));
+	}
+
+	public function test_tna_ebapi_shortcode()
+	{
+		$this->assertTrue(function_exists('tna_ebapi_shortcode'));
+	}
 }
