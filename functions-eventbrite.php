a7f6d58847845870f0df92820623efd86a1f066e
Chris Bishop <domingobishop@gmail.com>
2017-08-11 14:49:17 +0100

    wp_http if error or 404

--- functions-eventbrite.php
+++ functions-eventbrite.php
@@ -9,7 +9,6 @@ function tna_ebapi_url( $organiser, $category, $token ) {
 	$url = 'https://www.eventbriteapi.com/v3/events/search/?sort_by=date&organizer.id='.$organiser.$category.'&token='.$token.'&expand=ticket_classes';
 
 	return $url;
-
 }
 
 function tna_ebapi_get_eventbrite_json( $url ) {
@@ -20,7 +19,14 @@ function tna_ebapi_get_eventbrite_json( $url ) {
 
 	$request = new WP_Http;
 	$result = $request->request( $url );
-	$json = $result['body'];
+
+	if ( is_wp_error($result) ) {
+		$json = null;
+	} elseif ( wp_remote_retrieve_response_code($result) == '404' ) {
+		$json = null;
+	} else {
+		$json = $result['body'];
+	}
 
 	return $json;
 }
@@ -29,43 +35,50 @@ function tna_ebapi_events( $url, $number ) {
 
 	$json = tna_ebapi_get_eventbrite_json( $url );
 
-	$obj = json_decode($json);
-
-	$count = count($obj->events);
-
-	if ($number > $count) {
-		$number = $count-1;
-	} else {
-		$number = $number-1;
-	}
-
 	$html = '<div id="tna_ebapi_events" class="track-outbound"><ul class="tna-event-list">';
 
-	for ( $i=0 ; $i<=$number ; $i++ ) {
+	if ( $json ) {
 
-		$url        = $obj->events[$i]->url;
-		$title      = $obj->events[$i]->name->text;
-		$image      = $obj->events[$i]->logo->url;
-		$date       = date('l j F Y, H:i', strtotime($obj->events[$i]->start->local));
-		$status     = $obj->events[$i]->ticket_classes;
-		$tickets    = tna_ebapi_event_status( $status );
-		$online     = tna_ebapi_event_online( $obj->events[$i]->online_event );
+		$obj = json_decode( $json );
 
-		$html .= '<li>';
+		$count = count( $obj->events );
 
-		$html .= '<div class="event-img">'.$online;
+		if ( $number > $count ) {
+			$number = $count - 1;
+		} else {
+			$number = $number - 1;
+		}
 
-		$html .= '<a href="'.$url.'" target="_blank"><img src="'.$image.'" alt="'.$title.'"></a></div>';
+		for ( $i = 0; $i <= $number; $i ++ ) {
 
-		$html .= '<div class="event-text">';
+			$url     = $obj->events[ $i ]->url;
+			$title   = $obj->events[ $i ]->name->text;
+			$image   = $obj->events[ $i ]->logo->url;
+			$date    = date( 'l j F Y, H:i', strtotime( $obj->events[ $i ]->start->local ) );
+			$status  = $obj->events[ $i ]->ticket_classes;
+			$tickets = tna_ebapi_event_status( $status );
+			$online  = tna_ebapi_event_online( $obj->events[ $i ]->online_event );
 
-		$html .= '<p><span class="event-date">'.$date.'</span></p>';
+			$html .= '<li>';
 
-		$html .= '<h4><a href="'.$url.'" target="_blank">'.$title.'</a></h4>';
+			$html .= '<div class="event-img">' . $online;
 
-		$html .= '<p class="event-status">'.$tickets.'</p>';
+			$html .= '<a href="' . $url . '" target="_blank"><img src="' . $image . '" alt="' . $title . '"></a></div>';
 
-		$html .= '</div></li>';
+			$html .= '<div class="event-text">';
+
+			$html .= '<p><span class="event-date">' . $date . '</span></p>';
+
+			$html .= '<h4><a href="' . $url . '" target="_blank">' . $title . '</a></h4>';
+
+			$html .= '<p class="event-status">' . $tickets . '</p>';
+
+			$html .= '</div></li>';
+
+		}
+	} else {
+
+		$html .= '<h2>No events found</h2>';
 
 	}
 
