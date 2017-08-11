221a32af94937ae67a001563a51d1e49d6543495
Chris Bishop <domingobishop@gmail.com>
2017-08-11 16:49:36 +0100

    removed redundant tests

--- tests/FunctionsTest.php
+++ tests/FunctionsTest.php
@@ -21,27 +21,6 @@ class FunctionsTest extends PHPUnit_Framework_TestCase
 		$this->assertTrue(function_exists('tna_ebapi_settings_page'));
 	}
 
-	public function test_tna_ebapi_url()
-	{
-		$this->assertTrue(function_exists('tna_ebapi_url'));
-	}
-	public function test_tna_ebapi_get_eventbrite_json()
-	{
-		$this->assertTrue(function_exists('tna_ebapi_get_eventbrite_json'));
-	}
-	public function test_tna_ebapi_events()
-	{
-		$this->assertTrue(function_exists('tna_ebapi_events'));
-	}
-	public function test_tna_ebapi_event_status()
-	{
-		$this->assertTrue(function_exists('tna_ebapi_event_status'));
-	}
-	public function test_tna_ebapi_event_online()
-	{
-		$this->assertTrue(function_exists('tna_ebapi_event_online'));
-	}
-
 	public function test_tna_ebapi_css()
 	{
 		$this->assertTrue(function_exists('tna_ebapi_css'));
