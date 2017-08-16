bbb39ed203574c4703b6d36b71836d5185e6698e
Chris Bishop <domingobishop@gmail.com>
2017-08-16 12:29:20 +0100

    token error message help

--- functions-shortcode.php
+++ functions-shortcode.php
@@ -26,7 +26,10 @@ function tna_ebapi_shortcode( $atts ) {
 		$category = '&categories='.$cat;
 	}
 	if ($token=='') {
-		return '<h2>Eventbrite API token not found</h2>';
+		return '<h2>Eventbrite API token not found</h2>
+				<p>Please add token to settings page.
+				<a href="https://www.eventbrite.com/developer/v3/api_overview/authentication/#ebapi-getting-a-token">
+				For help please see Eventbrite documentation</a>.</p>';
 	}
 
 	$events = new Simple_Eventbrite_List;
