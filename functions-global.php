a2350bb41d928a8cc351835253463072b4fc8dd1
domingobishop <domingobishop@gmail.com>
2019-09-04 15:10:42 +0100

    https fix

--- functions-global.php
+++ functions-global.php
@@ -5,9 +5,18 @@
  */
 
 function tna_ebapi_css() {
-	wp_register_style('ebapi-styles', plugin_dir_url(__FILE__) . 'css/eventbrite.css.min', '', 2.0, 'all');
+	wp_register_style('ebapi-styles', make_url_https( plugin_dir_url(__FILE__) ) . 'css/eventbrite.css.min', '', 2.1, 'all');
 	global $post;
 	if (is_a($post, 'WP_Post') && has_shortcode($post->post_content, 'tna-eventbrite')) {
 		wp_enqueue_style('ebapi-styles');
 	}
+}
+
+function make_url_https( $url ) {
+
+    if ( strpos( $url, 'http:' ) !== false ) {
+        $url = str_replace('http:', 'https:', $url);
+        return $url;
+    }
+    return $url;
 }
